77c610f7e308d48cea1bcda4526a65fdf4712d61
Nazar Brych <nazar.brych.06@gmail.com>
2023-01-26 23:52:49 +0200

    Added repositories for interacting with main entities

---
+++ AASystem/DAL/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        Task<IEnumerable<Customer>> GetAllWithDetailsAsync();
+
+        Task<Customer> GetByIdWithDetailsAsync(int id);
+    }
+}
