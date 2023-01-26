b08b8e43cf2e1f31b5faba3a89c357da51dd447b
Nazar Brych <nazar.brych.06@gmail.com>
2023-01-26 23:44:46 +0200

    Added EqualityComparer for entities

---
+++ AASystem/AASystemTests/EqualityComparer.cs
@@ -0,0 +1,50 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AASystemTests
+{
+    internal class PersonEqualityComparer : IEqualityComparer<Person>
+    {
+        public bool Equals([AllowNull] Person x, [AllowNull] Person y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id
+                && x.Name == y.Name
+                && x.Surname == y.Surname
+                && x.BirthDate == y.BirthDate;
+        }
+
+        public int GetHashCode([DisallowNull] Person obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    internal class CustomerEqualityComparer : IEqualityComparer<Customer>
+    {
+        public bool Equals([AllowNull] Customer x, [AllowNull] Customer y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id
+                && x.PersonId == y.PersonId
+                && x.EmailAddress == y.EmailAddress
+                && x.PhoneNumber == y.PhoneNumber
+                && x.Password == y.Password
+                && x.Role == y.Role;
+        }
+
+        public int GetHashCode([DisallowNull] Customer obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
