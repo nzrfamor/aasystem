aebea1c78d870d926263a8da8893180d5a1a57de
Nazar Brych <nazar.brych.06@gmail.com>
2023-01-26 23:59:50 +0200

    Implemented IUnitOfWork interface

---
+++ AASystem/DAL/Data/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using DAL.Interfaces;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        readonly AASystemDbContext db;
+        ICustomerRepository customerRepository;
+        IPersonRepository personRepository;
+        public UnitOfWork(AASystemDbContext _db)
+        {
+            db = _db;
+        }
+        public ICustomerRepository CustomerRepository
+        {
+            get
+            {
+                return customerRepository ??= new CustomerRepository(db);
+            }
+        }
+
+        public IPersonRepository PersonRepository
+        {
+            get
+            {
+                return personRepository ??= new PersonRepository(db);
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            await db.SaveChangesAsync();
+        }
+    }
+}
