5d6043abb67550bd56ea2680f2390c78cd6e025c
Nazar Brych <nazar.brych.06@gmail.com>
2023-01-26 23:58:26 +0200

    Added interface that unit work with all repositories

---
+++ AASystem/DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        ICustomerRepository CustomerRepository { get; }
+        IPersonRepository PersonRepository { get; }
+
+        Task SaveAsync();
+    }
+}
