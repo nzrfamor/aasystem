d4b93f87a0b49038400a2fba49b83be798029576
Nazar Brych <nazar.brych.06@gmail.com>
2023-01-26 23:59:12 +0200

    Added data context

---
+++ AASystem/DAL/Data/AASystemDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Data
+{
+    public class AASystemDbContext : DbContext
+    {
+        public AASystemDbContext(DbContextOptions<AASystemDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasOne(c => c.Person)
+                .WithOne()
+                .HasForeignKey<Customer>(p => p.PersonId);
+        }
+    }
+}
