3a62ee5e8b51c504ba6c6546fd511020e1058b68
Nazar Brych <nazar.brych.06@gmail.com>
2023-01-26 23:49:01 +0200

    Added Person entity class

---
+++ AASystem/DAL/Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Entities.AbstractEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public enum Roles
+    {
+        user,
+
+        admin
+    }
+    public class Customer : BaseEntity
+    {
+        public int PersonId { get; set; }
+        public Roles Role { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public Person Person { get; set; }    
+    }
+}
